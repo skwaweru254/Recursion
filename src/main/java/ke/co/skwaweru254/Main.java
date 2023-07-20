96261d00f537d4be42bb82ae0ea050042a6beca8
skwaweru254 <skwaweru254@gmail.com>
2023-07-20 10:26:53 +0300

    Initial commit

---
+++ src/main/java/ke/co/skwaweru254/Main.java
@@ -0,0 +1,31 @@
+package ke.co.skwaweru254;
+
+public class Main {
+    public static void main(String[] args) {
+        printing();
+
+    }
+
+    static void printing(){
+        System.out.println("Printing Method\n");
+        writing();
+    }
+
+    static void writing(){
+        System.out.println("Writing Method\n");
+        eating();
+
+
+    }
+    static void eating(){
+        System.out.println("Eating Method\n");
+        working();
+
+
+    }
+    static void working(){
+        System.out.println("Working Method");
+
+
+    }
+}
