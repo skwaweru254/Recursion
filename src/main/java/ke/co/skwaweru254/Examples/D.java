5de5f6423d36e5b37e79d326e35ef1e882cc60ae
skwaweru254 <skwaweru254@gmail.com>
2023-07-31 09:41:18 +0300

    Fibonacci restructuring.

--- src/main/java/ke/co/skwaweru254/Examples/D.java
+++ src/main/java/ke/co/skwaweru254/Examples/D.java
@@ -1,21 +1,21 @@
 package ke.co.skwaweru254.Examples;
 
 public class D {
-    static int n1=0,n2=1,n3=0;
+    static int n0=0,n1=1,n2=1;
     static void fibonacci(int count){
         if(count>0){
-            n3 = n1 + n2;
+            n2 = n0 + n1;
+            n0 = n1;
             n1 = n2;
-            n2 = n3;
-            System.out.print(" "+n3);
+            System.out.print(" "+n2);
 
             fibonacci(count-1);
         }
     }
 
     public static void main(String[] args) {
-        int count=111;
-        System.out.print(n1+" "+n2);//printing 0 and 1
-        fibonacci(count-2);//n-2 because 2 numbers are already printed
+        int count=7;
+        System.out.print(n0+" "+n1);//printing 0 and 1
+        fibonacci(count-1);//n-2 because 2 numbers are already printed
     }
 }
