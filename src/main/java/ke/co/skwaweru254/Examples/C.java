01f6469e0b2826f6c4644f7e6c553082747ffe74
skwaweru254 <skwaweru254@gmail.com>
2023-07-31 10:01:49 +0300

    Factorial restructuring.

--- src/main/java/ke/co/skwaweru254/Examples/C.java
+++ src/main/java/ke/co/skwaweru254/Examples/C.java
@@ -4,19 +4,20 @@ public class C {
     public static void main(String[] args) {
 
         int ans = factorial(5);
-        System.out.println("Factorial: " + ans);
+        System.out.println("Factorial result is "  + ans);
 
 
     }
 
     static int factorial(int y) {
-        System.out.println("Number: " + y);
+        System.out.println(y+" multiply by factorial of "+(y-1));
 
         if (y == 1) {
 
             return 1;
-        } else
-            return y * factorial(y - 1);
+        }
+
+        return y * factorial(y - 1);
 
     }
 }
